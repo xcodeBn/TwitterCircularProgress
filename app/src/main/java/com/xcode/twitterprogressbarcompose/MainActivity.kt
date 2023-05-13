cf93efc7e396357f73033f60f37ec2c34d36db02
xcodebn <xcode.bazzoun@gmail.com>
2023-05-13 17:34:51 +0300

    -fixed implementation bugs -updated version
    Signed-off-by: xcodebn <xcode.bazzoun@gmail.com>

--- app/src/main/java/com/xcode/twitterprogressbarcompose/MainActivity.kt
+++ app/src/main/java/com/xcode/twitterprogressbarcompose/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.xcode.twittercircularprogress.TwitterCircularProgress
 import com.xcode.twitterprogressbarcompose.ui.theme.TwitterProgressBarComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,6 +57,7 @@ fun GreetingPreview() {
                 modifier = Modifier,
                 size = 50.dp
             )
+
         }
 
 
